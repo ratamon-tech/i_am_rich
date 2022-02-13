a991d9c3c92e59b741abd3e10939f23f853fa010
ratamon-tech <ratamon91@gmail.com>
2022-02-13 09:59:53 +0900

    add a genbaneko

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const GENBANEKO='https://booth.pximg.net/30829f4a-b2ee-4814-9ff3-c5e038d7bc0c/i/2114006/15fe8b65-15d8-41c6-b10b-c314a6397396_base_resized.jpg';
+
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
@@ -8,6 +10,12 @@ void main() {
         appBar: AppBar(
           title: Text('I AM Rich'),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(GENBANEKO),
+          ),
         ),
       ),
     ),
