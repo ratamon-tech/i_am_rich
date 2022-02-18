f89437279c598719955ef20386ec628050717247
ratamon-tech <ratamon91@gmail.com>
2022-02-18 23:13:05 +0900

    assetsを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-const GENBANEKO='https://booth.pximg.net/30829f4a-b2ee-4814-9ff3-c5e038d7bc0c/i/2114006/15fe8b65-15d8-41c6-b10b-c314a6397396_base_resized.jpg';
-
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
@@ -21,7 +19,7 @@ void main() {
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage(GENBANEKO),
+            image: AssetImage('images/genbaneko.jpeg'),
           ),
         ),
       ),
